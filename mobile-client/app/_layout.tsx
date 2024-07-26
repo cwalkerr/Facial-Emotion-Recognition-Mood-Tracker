5c8274ed005dca0ae77c0f32c1f3aa32f53d2607
cwalkerr <connor.walker0@proton.me>
2024-07-26 22:20:57 +0100

    init gluestack ui, add config files

--- mobile-client/app/_layout.tsx
+++ mobile-client/app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import "'@/global.css'";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <GluestackUIProvider mode="light">
+      <Stack>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
